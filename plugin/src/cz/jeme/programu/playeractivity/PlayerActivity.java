acf91907f9534f0272f92b67c89e0012106afdf6
XD_CZ <mandlemankiller@email.cz>
2022-04-20 20:02:52 +0200

    Initial plugin commit

---
+++ plugin/src/cz/jeme/programu/playeractivity/PlayerActivity.java
@@ -0,0 +1,14 @@
+package cz.jeme.programu.playeractivity;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PlayerActivity extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
