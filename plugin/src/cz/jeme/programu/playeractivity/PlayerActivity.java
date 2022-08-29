9d314aa6f9208af3dbae4e2cb9596ab099c1fe27
XD_CZ <mandlemankiller@email.cz>
2022-08-29 16:36:14 +0200

    Remove bukkit source code

--- plugin/src/cz/jeme/programu/playeractivity/PlayerActivity.java
+++ plugin/src/cz/jeme/programu/playeractivity/PlayerActivity.java
@@ -1,15 +1,33 @@
 package cz.jeme.programu.playeractivity;
 
+import java.io.File;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PlayerActivity extends JavaPlugin {
+public class PlayerActivity extends JavaPlugin implements Listener {
 
 	@Override
 	public void onEnable() {
-		
+		PluginManager pm = Bukkit.getServer().getPluginManager();
+		pm.registerEvents(this, this);
+		File dataDir = getDataFolder();
+
+		if (!dataDir.exists()) {
+			dataDir.mkdir();
+		}
 	}
 
 	@Override
 	public void onDisable() {
+	}
+
+	@EventHandler
+	public void onBlockBreakEvent(BlockBreakEvent event) {
+		
 	}
 }
